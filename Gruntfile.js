d67b4b377ff5ee1fbf4dc4dcfb14ed0740f692b9
gouflv <lv.gouf@gmail.com>
2014-08-05 16:45:58 +0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,66 +8,30 @@
 
 'use strict';
 
-module.exports = function(grunt) {
+module.exports = function(grunt) {  
+	require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      },
-    },
+	grunt.initConfig({
 
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
+		view_dir: '/Users/gouflv/Develops/php_project/mbp.com_2014/Application/Home/View/default',
+		static_dir: '/Users/gouflv/Develops/php_project/mbp.com_2014/Public/statics',
 
-    // Configuration to be run (and then tested).
-    static_bump: {
-      default_options: {
-        options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-    },
+		static_bump: {
+			options: {
+				__STATICS__: '<%= static_dir %>',
+				view_dir: '<%= view_dir %>'
+			},
+			css: {
+				src: ['<%= static_dir %>/css/**/*.css']
+			},
+			js: {
+				src: ['<%= static_dir %>/dist/*.js']
+			}
+		}
+	});
 
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
-
-  });
-
-  // Actually load this plugin's task(s).
-  grunt.loadTasks('tasks');
-
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'static_bump', 'nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+	grunt.registerTask('default', [
+		'static_bump'
+	]);
 
 };
