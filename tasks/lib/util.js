52f9ba4383042c385122ddef22e6006554822b7e
gouflv <lv.gouf@gmail.com>
2014-08-05 16:48:13 +0800

    Update util.js

--- tasks/lib/util.js
+++ tasks/lib/util.js
@@ -10,3 +10,11 @@ exports.fhash = function(filepath) {
 	var md5 = require('MD5')
 	return md5(fs.readFileSync(filepath)).substr(0, 8)
 }
+
+exports.orgin_file_path = function(fname) {
+    var urls = fname.split('?')
+    if(urls.length >= 2) {
+        return urls[0]
+    }
+    return fname
+}
